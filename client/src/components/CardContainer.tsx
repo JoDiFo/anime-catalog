5c491f7ead1254c67e7a17ba836d9141fb591463
sasafokin2@gmail.com <sasafokin2@gmail.com>
2024-01-10 17:08:34 +0300

    add: types add: select items per page

--- client/src/components/CardContainer.tsx
+++ client/src/components/CardContainer.tsx
@@ -1,11 +1,37 @@
 import { AnimeCard } from "./index";
 
-function CardContainer({ items }: any) {
+interface AnimeList {
+  items: Anime[]
+}
+
+type Anime = {
+  
+  id: string;
+  title: string;
+  type: string;
+  episodes: number;
+  status: string;
+  animeSeason: AnimeSeason;
+  picture: string;
+  synonyms: string[];
+  tags: string[];
+}
+
+type AnimeSeason = {
+  season: string;
+  year: number;
+}
+
+function CardContainer({ items }: AnimeList) {
   return (
     <div className="profile-page__list card-container">
       {items &&
         items.map((item: any) => (
-          <AnimeCard key={item.id} animeThumbnail={item.picture} title={item.title} />
+          <AnimeCard
+            key={item.id}
+            animeThumbnail={item.picture}
+            title={item.title}
+          />
         ))}
     </div>
   );
--- client/src/components/Content.tsx
+++ client/src/components/Content.tsx
@@ -4,45 +4,51 @@ import ReactPaginate from "react-paginate";
 import { CardContainer } from "../components";
 
 function Content({ items }: any) {
-//   const [currentItems, setCurrentItems] = useState([]);
-  const [itemOffset, setItemOffset] = useState(0);
-//   const [pageCount, setPageCount] = useState(1);
-  const itemsPerPage = 35;
+  const selectOptions = [
+    { value: 35, text: "35" },
+    { value: 70, text: "70" },
+    { value: 120, text: "120" },
+    { value: 200, text: "200" },
+  ];
 
-  const endOffset = itemOffset + itemsPerPage;
-  const newCurrentItems = items.slice(itemOffset, endOffset);
-//   setCurrentItems(newCurrentItems);
-  const newPageCount = Math.ceil(items.length / itemsPerPage);
-//   setPageCount(newPageCount);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(selectOptions[0].value);
+//   const itemsPerPage = 35;
+  const endOffset = itemOffset + selectedOption;
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / selectedOption);
 
   const handlePageClick = (event: any) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
+    const newOffset = (event.selected * selectedOption) % items.length;
     setItemOffset(newOffset);
+  };
+
+  const handleSelectChange = (event: any) => {
+    setSelectedOption(event.target.value);
   };
 
   return (
     <>
-      <CardContainer items={newCurrentItems} />
+      <CardContainer items={currentItems} />
       <div className="content-navigation">
         <select
           name="number-of-titles"
           id="number-of-titles"
           className="content-navigation__items-per-page"
+          onChange={(event) => handleSelectChange(event)}
         >
-          <option value="25">25</option>
-          <option value="50">50</option>
-          <option value="75">75</option>
-          <option value="100">100</option>
+          {selectOptions.map((item) => (
+            <option key={`${item.value}_${item.text}`} value={item.value}>
+              {item.text}
+            </option>
+          ))}
         </select>
         <ReactPaginate
           breakLabel="..."
           nextLabel=">"
           onPageChange={handlePageClick}
           pageRangeDisplayed={4}
-          pageCount={newPageCount}
+          pageCount={pageCount}
           previousLabel="<"
           renderOnZeroPageCount={null}
           containerClassName="content-navigation__page-number"
