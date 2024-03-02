2285aa7dbed938dd227b898cffeb52aa8f0c1fe0
alexfokin <sasafokin2@gmail.com>
2024-03-02 17:52:27 +0300

    refactor: change file structure & migrate from express-graphql to graphql-http

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,28 @@
 import express from "express";
 import cors from "cors";
 
-import { graphqlHTTP } from "express-graphql";
+import { createHandler } from "graphql-http/lib/use/express";
 import { schema } from "./graphql/schema.js";
 import { root } from "./graphql/root.js";
+import { ruruHTML } from "ruru/server";
 
 const PORT = 8000;
 
 const app = express();
 app.use(cors());
 
-app.use(
+app.all(
   "/graphql",
-  graphqlHTTP({
-    graphiql: true,
+  createHandler({
     schema,
     rootValue: root,
   })
 );
+
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
 
 app.listen(PORT, () =>
   console.log(`Listening on port http://localhost:${PORT}`)
