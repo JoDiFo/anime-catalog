b987d43db73f3e88f04ae9e1c28c26a77c5fa23d
alexfokin <sasafokin2@gmail.com>
2024-03-30 11:38:09 +0300

    feat: add loading animation

--- client/src/widgets/Loading/ui/Loading.tsx
+++ client/src/widgets/Loading/ui/Loading.tsx
@@ -1,7 +1,10 @@
+import { Loader } from "@/shared/ui/Loader/Loader";
 import cls from "./Loading.module.scss";
 
-function Loading() {
-  return <div className={cls.Loading}>Loading...</div>;
+export function Loading() {
+  return (
+    <div className={cls.Loading}>
+      <Loader />
+    </div>
+  );
 }
-
-export default Loading;
